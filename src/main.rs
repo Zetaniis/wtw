a534a16f43980a3660bc7fb0b03334fc630f689a
Zetaniis <63559030+Zetaniis@users.noreply.github.com>
2024-02-11 21:02:37 +0100

    Proposed arg parser struct in clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::borrow::Borrow;
-use std::collections::HashSet;
+// use std::borrow::Borrow;
+// use std::collections::HashSet;
 use std::env;
 use std::collections as coll;
-use std::ffi::OsStr;
+// use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::fs;
+use clap::builder::ValueParserFactory;
+// use clap::ValueEnum;
 // use std::path;
 // use std::path;
 // use std::ffi;
 // use std::str::FromStr;
 // use serde_json;
 use serde_json;
+use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Ord)]
 #[derive(PartialOrd)]
@@ -110,52 +114,142 @@ fn get_config_string_data( current_term_cfg_name_path : &(TerminalVersion, OsStr
     return contents;
 }
 
-fn change_bg_image(path_to_img : &OsString, config_json_data : &mut serde_json::Value) {
-    fs::metadata(path_to_img)
-        .expect("Incorrect path. File not found.");
+fn change_bg_image(path_to_img : &OsString, config_json_data : &mut serde_json::Value) -> Result<(), String> {
+    // TODO: this is too strict, I think. Check if this works with URIs
+    if fs::metadata(path_to_img).is_err() {
+        return Err("Incorrect path. File not found.".to_string());
+    }
+        // .expect("Incorrect path. File not found.");
 
     config_json_data["profiles"]["defaults"]["backgroundImage"] = path_to_img.clone().into_string().unwrap().into();
+    return Ok(());
 }
 
-fn change_bg_image_opacity(opacity_value : f64, config_json_data : &mut serde_json::Value) {
-    if opacity_value < 0.0 || opacity_value > 1.0 {
-        panic!("Incorrect image opacity value. Correct inputs in range 0.0-1.0")
+fn change_bg_image_opacity(opacity_value : i32, config_json_data : &mut serde_json::Value) -> Result<(), String> {
+    if opacity_value < 0 || opacity_value > 100 {
+        return Err(format!("Incorrect image opacity value. Correct inputs in range 0-100"));
+        // panic!("Incorrect image opacity value. Correct inputs in range 0.0-1.0")
     }
 
-    config_json_data["profiles"]["defaults"]["backgroundImageOpacity"] = opacity_value.into();
+
+    config_json_data["profiles"]["defaults"]["backgroundImageOpacity"] = ((opacity_value as f64) / 100.0).into();
+    return Ok(());
 }
 
-fn change_bg_image_alignment(aligment_type : &String, config_json_data : &mut serde_json::Value) {
+fn change_bg_image_alignment(aligment_type : &String, config_json_data : &mut serde_json::Value) -> Result<(), String> {
     let alignment_types = coll::HashSet::from(["center", "left", "top", "right", "bottom", "topLeft", "topRight", "bottomLeft", "bottomRight"]);   
 
     if !alignment_types.contains(aligment_type.as_str()) {
-        panic!("Incorrect aligment type. Possible types: {:#?}", alignment_types);
+        return Err(format!("Incorrect aligment type. Possible types: {:#?}", alignment_types));
+        // panic!("Incorrect aligment type. Possible types: {:#?}", alignment_types);
     }
 
     config_json_data["profiles"]["defaults"]["backgroundImageAlignment"] = aligment_type.clone().into();
+    return Ok(());
 }
 
-fn change_bg_image_stretch_mode(stretch_mode : &String,  config_json_data : &mut serde_json::Value) {
+fn change_bg_image_stretch_mode(stretch_mode : & String,  config_json_data : & mut serde_json::Value) -> Result<(), String> {
     let stretch_modes = coll::HashSet::from(["none", "fill", "uniform", "uniformToFill"]);
 
     if !stretch_modes.contains(stretch_mode.as_str()) {
-        panic!("Incorrect stretch mode. Possible types: {:#?}", stretch_modes);
+        return Err(format!("Incorrect stretch mode. Possible types: {:#?}", stretch_modes));
+        // panic!("Incorrect stretch mode. Possible types: {:#?}", stretch_modes);
     }
     
     config_json_data["profiles"]["defaults"]["backgroundImageStretchMode"] = stretch_mode.clone().into();
+    return Ok(());
 }
 
-fn change_term_opacity(opacity_value : i8,  config_json_data : &mut serde_json::Value) {
+fn change_term_opacity(opacity_value : i8,  config_json_data : &mut serde_json::Value) -> Result<(), String> {
     if opacity_value < 0 || opacity_value > 100 {
-        panic!("Incorrect terminal opacity value. Correct inputs in range 0-100")
+        return Err("Incorrect terminal opacity value. Correct inputs in range 0-100".to_string());
+        // panic!("Incorrect terminal opacity value. Correct inputs in range 0-100")
     }
 
     config_json_data["profiles"]["defaults"]["opacity"] = opacity_value.into();
+    return Ok(());
 }
 
+// Arg parser struct
+#[derive(Parser)]
+#[command(version, about, long_about = None, arg_required_else_help = true)]
+struct Cli {
+    // /// Optional name to operate on
+    // name: Option<String>,
+
+    // /// Sets a custom config file
+    // #[arg(short, long, value_name = "FILE")]
+    // config: Option<PathBuf>,
+
+    // /// Turn debugging information on
+    // #[arg(short, long, action = clap::ArgAction::Count)]
+    // debug: u8,
+
+    // #[command(subcommand)]
+    // command: Option<Commands>,
+
+    /// Choose terminal version. Default will act on the first found. 
+    #[arg(short, long, action = clap::ArgAction::Append)]
+    terminal_version: Option<String>,
+
+    /// Use image as background
+    #[arg(short, long, value_name = "PATH_TO_IMAGE")]
+    path: Option<Option<PathBuf>>,
+
+    /// Change opacity of the image (% value)
+    #[arg(short = 'o', long, action = clap::ArgAction::Append)]
+    image_opacity: Option<u8>,
+
+    /// Change alignment type of background image (% value)
+    #[arg(short, long, value_name = "ALIGNMENT_TYPE")]
+    align: Option<String>,
+
+    /// Change stretch mode of background image
+    #[arg(short, long, value_name = "STRETCH_MODE")]
+    stretch: Option<String>,
+
+    /// Change opacity of the terminal
+    #[arg(short = 'O', long, action = clap::ArgAction::Append)]
+    terminal_opacity: Option<u8>,
+
+    /// Choose a random image from paths inputted or a folder
+    #[arg(short = 'r', long, action = clap::ArgAction::Append)]
+    random_image: Option<Vec<String>>
+
+
+    // TODO perform conversion from 0-100 to 0-1 for appropriate features
+}
+
+// #[derive(Clone)]
+// struct VecStringOrString {
+//     v: Vec<String>,
+//     s: String
+// }
+
+// #[derive(Subcommand)]
+// enum Commands {
+//     /// does testing things
+//     Test {
+//         /// lists test values
+//         #[arg(short, long)]
+//         list: bool,
+//     },
+// }
+
+// fn prepare_arg_parser() -> Command {
+
+//     return matches;
+// }
+
 fn main() -> Result<(), String> {
-    let args : Vec<String> = env::args().collect();
-    dbg!(args);
+    // let args : Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let cli = Cli::parse();
+
+
+    
+
 
     let versions_to_paths_mapping: coll::BTreeMap<TerminalVersion, OsString> = prep_version_path_struct();
 
@@ -169,7 +263,7 @@ fn main() -> Result<(), String> {
     let mut config_json_data = get_config_json(&config_string_data);
 
 
-    println!("{:#?}", config_json_data);
+    // println!("{:#?}", config_json_data);
 
     // change_bg_image_opacity(0.9, &mut config_json_data);
  
@@ -182,7 +276,7 @@ fn main() -> Result<(), String> {
     // change_term_opacity(20, &mut config_json_data);
 
 
-    println!("{:#?}", config_json_data);
+    // println!("{:#?}", config_json_data);
 
 
 
